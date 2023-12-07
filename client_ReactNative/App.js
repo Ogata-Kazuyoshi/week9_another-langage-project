aa30217193e19bca52d0991510fe37d3b1d675d1
Ogata-Kazuyoshi <kazuyoshi_ogata_ab@mail.toyota.co.jp>
2023-12-07 18:27:50 +0900

    UPDATE : ユーザー画面で、getALL ,getByUserの動作確認OK!!

--- client_ReactNative/App.js
+++ client_ReactNative/App.js
@@ -9,56 +9,6 @@ import UserScreen from './UserScreen';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
-  const getAllHandler = async () => {
-    const res = await dbApi.getDB();
-    console.log('resAll : ', res.data);
-  };
-  const getSingleHandler = async () => {
-    const res = await dbApi.getSingleDB(1);
-    console.log('resSingle : ', res.data);
-  };
-  const createNewHandler = async () => {
-    const sendDate = {
-      //newCreateはtodo.goの20-31行のCreateTodoを通るので、Todoと同じ型でないといけない。
-      content: 'aaa',
-      // created_at: new Date(),
-      UserId: 5,
-    };
-    const res = await dbApi.createNew(sendDate);
-    // console.log('resSingle : ', res);
-    Alert.alert(res.data.message, '', [
-      {
-        text: 'OK',
-        onPress: () => {},
-      },
-    ]);
-  };
-  const updateHandler = async () => {
-    const sendDate = {
-      //Updateはそのまま直接DBにknex likeなやつでアクセスに行くので、カラム名でないといけない。
-      //todo.goのUPDATEtodo
-      content: '1234',
-      user_id: 5,
-    };
-    const res = await dbApi.update(sendDate, 4);
-    console.log('Update : ', res);
-    Alert.alert(res.data.message, '', [
-      {
-        text: 'OK',
-        onPress: () => {},
-      },
-    ]);
-  };
-  const deleteHandler = async () => {
-    const res = await dbApi.delete(11);
-    console.log('Update : ', res);
-    Alert.alert(res.data.message, '', [
-      {
-        text: 'OK',
-        onPress: () => {},
-      },
-    ]);
-  };
   return (
     <NavigationContainer>
       <Stack.Navigator>
--- client_ReactNative/UserScreen.jsx
+++ client_ReactNative/UserScreen.jsx
@@ -1,10 +1,66 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import dbApi from './api/dbApi';
 
 const UserScreen = () => {
+  const getAllHandler = async () => {
+    const res = await dbApi.getDB();
+    console.log('resAll : ', res.data);
+  };
+  const getByuserHandler = async () => {
+    const res = await dbApi.getSingleDB(1);
+    console.log('resSingle : ', res.data);
+  };
+  const createNewHandler = async () => {
+    const sendDate = {
+      //newCreateはtodo.goの20-31行のCreateTodoを通るので、Todoと同じ型でないといけない。
+      content: 'aaa',
+      // created_at: new Date(),
+      UserId: 5,
+    };
+    const res = await dbApi.createNew(sendDate);
+    // console.log('resSingle : ', res);
+    Alert.alert(res.data.message, '', [
+      {
+        text: 'OK',
+        onPress: () => {},
+      },
+    ]);
+  };
+  const updateHandler = async () => {
+    const sendDate = {
+      //Updateはそのまま直接DBにknex likeなやつでアクセスに行くので、カラム名でないといけない。
+      //todo.goのUPDATEtodo
+      content: '1234',
+      user_id: 5,
+    };
+    const res = await dbApi.update(sendDate, 4);
+    console.log('Update : ', res);
+    Alert.alert(res.data.message, '', [
+      {
+        text: 'OK',
+        onPress: () => {},
+      },
+    ]);
+  };
+  const deleteHandler = async () => {
+    const res = await dbApi.delete(11);
+    console.log('Update : ', res);
+    Alert.alert(res.data.message, '', [
+      {
+        text: 'OK',
+        onPress: () => {},
+      },
+    ]);
+  };
   return (
     <View>
       <Text>ユーザー画面</Text>
+      <Button title="GetAll" onPress={getAllHandler} />
+      <Button title="GetSingle" onPress={getByuserHandler} />
+      <Button title="NewPost" onPress={createNewHandler} />
+      <Button title="Update" onPress={updateHandler} />
+      <Button title="delete" onPress={deleteHandler} />
     </View>
   );
 };
